e1b75fdc4e01991c496e4ee35867fdd9a02611af
aaron-lamers <1218389@student.roc-nijmegen.nl>
2025-11-27 12:44:41 +0100

    js testen

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+console.log("Hallo mijn naam is Aaron");
+console.log("69420");
+console.log(5 + 7);
+
+var naam = "Aaron";
+var leeftijd = 20;
+var isStudent = true;
+
+console.log("Mijn naam is " + naam + ", ik ben " + leeftijd + " jaar oud.");
